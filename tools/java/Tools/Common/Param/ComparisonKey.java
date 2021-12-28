0f6ded527374cf19c6936d80cd2cb90c9131be40
aitianyu macbook <aitianyu.cn@outlook.com>
2021-12-29 00:18:36 +0800

    copy implementation from dotnet to Java
        parameters handling implementation in Java.
    
    branch: dev
    user: aitianyu.cn

--- tools/java/Tools/Common/Param/ComparisonKey.java
+++ tools/java/Tools/Common/Param/ComparisonKey.java
@@ -2,5 +2,51 @@ package Tools.Common.Param;
 
 public class ComparisonKey
 {
+    private String _FullName;
+    private char _SingleName;
 
+    public ComparisonKey(String fname, char sname)
+    {
+        this._FullName = fname;
+        this._SingleName = sname;
+    }
+
+    public ComparisonKey(ComparisonKey key)
+    {
+        this._FullName = key._FullName;
+        this._SingleName = key._SingleName;
+    }
+
+    public String GetFullName()
+    {
+        return this._FullName;
+    }
+
+    public char GetSingleName()
+    {
+        return this._SingleName;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this.getClass() != obj.getClass())
+            return false;
+
+        ComparisonKey cobj = (ComparisonKey) obj;
+        return cobj._FullName.equals(this._FullName)
+                || cobj._SingleName == this._SingleName;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("%s-%c", this._FullName, this._SingleName);
+    }
 }
