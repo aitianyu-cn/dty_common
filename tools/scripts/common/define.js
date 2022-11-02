6df35967a26c856b3acd2b1da3a77d54e22ba267
tianyu 12900k <ysydty@outlook.com>
2022-11-03 01:59:03 +0800

    refactor the auto script
    	extract the i18n script to common and prepare to the serialized
    	job for target building.
    
    branch: dev
    user: ysydty

--- tools/scripts/common/define.js
+++ tools/scripts/common/define.js
@@ -1,7 +1,7 @@
 /**@format */
 
 module.exports = {
-    TIANYU_NATIVE_NATIVE_RES_PATH: "../../native/res",
+    TIANYU_NATIVE_NATIVE_RES_PATH: "../../../native/res",
 
     TIANYU_NATIVE_NATIVE_RES_I18N_NAME: "i18n",
 };
