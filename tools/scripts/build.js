b9976d37388c7b84901c0f8b85c9cbcc2e6309ef
tianyu 12900k <ysydty@outlook.com>
2022-11-02 20:25:12 +0800

    prepare build script
    	prepare the project source paths and add npm run build
    
    branch: dev
    user: ysydty

--- tools/scripts/build.js
+++ tools/scripts/build.js
@@ -2,4 +2,8 @@
 
 const path = require("path");
 
+const buildConfiguration = require("../../config/build.json");
+
 const PROJECT_BASE_PATH = path.resolve(__dirname, "../..");
+const PROJECT_SOURCE_ENTRY = path.resolve(PROJECT_BASE_PATH, buildConfiguration.baseURL);
+const PROJECT_BUILD_TARGET = path.resolve(PROJECT_BASE_PATH, buildConfiguration.output);
